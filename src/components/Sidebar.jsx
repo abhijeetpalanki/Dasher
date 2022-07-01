7e1d1eb800884616617a0851017b96864335b5d5
abhijeetpalanki <ajpalanki@gmail.com>
2022-07-01 18:00:10 -0500

    context api for global use

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,10 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
@@ -20,7 +21,7 @@ const Sidebar = () => {
           <div className="flex items-center justify-between">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={() => setActiveMenu(false)}
               className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Dasher</span>
@@ -28,7 +29,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() =>
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+                }
                 className="block p-3 mt-4 text-xl rounded-full hover:bg-light-gray md:hidden"
               >
                 <MdOutlineCancel />
